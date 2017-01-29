3666e62ff8a0287ac2cb2a9dd09839122b2b535f
Jackie Wellons <jackie@localhost.localdomain>
2017-01-29 11:16:57 -0800

    adding init.el file for emacs

---
+++ elpa/abyss-theme-20160420.512/abyss-theme-autoloads.el
@@ -0,0 +1,29 @@
+;;; abyss-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "abyss-theme" "abyss-theme.el" (22626 6861
+;;;;;;  257787 336000))
+;;; Generated autoloads from abyss-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(autoload 'abyss-theme "abyss-theme" "\
+Load abyss-theme.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("abyss-theme-pkg.el") (22626 6861 328957
+;;;;;;  490000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; abyss-theme-autoloads.el ends here
