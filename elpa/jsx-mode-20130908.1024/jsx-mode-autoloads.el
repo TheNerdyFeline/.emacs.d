62b3d21af6d7c572529f9e8e2d10463d18b29439
Jackie Wellons <jackie@jackie-dell.jackie>
2017-06-03 12:28:34 -0700

    added jsx-mode

---
+++ elpa/jsx-mode-20130908.1024/jsx-mode-autoloads.el
@@ -0,0 +1,22 @@
+;;; jsx-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "jsx-mode" "jsx-mode.el" (22835 3268 756018
+;;;;;;  75000))
+;;; Generated autoloads from jsx-mode.el
+
+(autoload 'jsx-mode "jsx-mode" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; jsx-mode-autoloads.el ends here
